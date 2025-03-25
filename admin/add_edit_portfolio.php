e26b3905852a45e8f2f67c64fefdd62e4ec4c191
Dio-Damar-Danendra-Portofolio <diodamar14102000@gmail.com>
2025-03-25 08:37:27 +0700

    pesan commit atau perubahan

--- admin/add_edit_portfolio.php
+++ admin/add_edit_portfolio.php
@@ -60,7 +60,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title><?php echo isset($_GET['id']) ? 'Edit' : 'Add' ?> Portfolio - Portofolio Manager</title>
+    <title><?php echo isset($_GET['idEdit']) ? 'Edit' : 'Add' ?> Portfolio - Portofolio Manager</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -71,7 +71,7 @@
         <div class="col-lg-12">
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title"><?php echo isset($_GET['id']) ? 'Edit' : 'Add' ?> Portfolio</h5>
+              <h5 class="card-title"><?php echo isset($_GET['idEdit']) ? 'Edit' : 'Add' ?> Portfolio</h5>
               <form action="" method="post" enctype="multipart/form-data">
                 <div class="row mb-3">
                     <div class="col-sm-2">
